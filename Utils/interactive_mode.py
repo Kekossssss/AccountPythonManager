50d16c49d834d1de7b5dca476af1773a03f0c7eb
Kekossssss <kevinguilloux.kg@gmail.com>
2026-05-26 21:33:43 +0100

    First draft of an interactive mode, with basic features included, but multilanguage support is not yet supported here

---
+++ Utils/interactive_mode.py
@@ -0,0 +1,296 @@
+import os
+import math
+from termcolor import colored
+from Classes.account_manager_class import Account_manager
+
+########################################################################
+## TITLES                                                             ##
+########################################################################
+ONE_LINE_TITLE = [
+    "  /$$$$$$$                  /$$                       /$$           /$$      /$$",
+    " | $$__  $$                | $$                      | $$          | $$$    /$$$",
+    " | $$  \ $$ /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$        | $$$$  /$$$$  /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$",
+    " | $$$$$$$ | $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/        | $$ $$/$$ $$ |____  $$| $$__  $$ |____  $$ /$$__  $$ /$$__  $$ /$$__  $$",
+    " | $$__  $$| $$  | $$| $$  | $$| $$  \ $$| $$$$$$$$  | $$          | $$  $$$| $$  /$$$$$$$| $$  \ $$  /$$$$$$$| $$  \ $$| $$$$$$$$| $$  \__/",
+    " | $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$_____/  | $$ /$$      | $$\  $ | $$ /$$__  $$| $$  | $$ /$$__  $$| $$  | $$| $$_____/| $$",
+    " | $$$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/      | $$ \/  | $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$",
+    " |_______/  \______/  \_______/ \____  $$ \_______/   \___/        |__/     |__/ \_______/|__/  |__/ \_______/ \____  $$ \_______/|__/",
+    "                                /$$  \ $$                                                                      /$$  \ $$",
+    "                               |  $$$$$$/                                                                     |  $$$$$$/",
+    "                                \______/                                                                       \______/",
+    "                                                |---------------------------------|",
+    "                                                | VERSION 1.0 - Made by Kekosssss |",
+    "                                                |---------------------------------|"
+]
+ONE_LINE_TITLE_REQUIRED_WIDTH = max([len(ONE_LINE_TITLE[i]) for i in range(len(ONE_LINE_TITLE))])
+ONE_LINE_TITLE_REQUIRED_HEIGHT = len(ONE_LINE_TITLE)
+
+TWO_LINE_TITLE = [
+    "  /$$$$$$$                  /$$                       /$$",
+    " | $$__  $$                | $$                      | $$",
+    " | $$  \ $$ /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$",
+    " | $$$$$$$ | $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/",
+    " | $$__  $$| $$  | $$| $$  | $$| $$  \ $$| $$$$$$$$  | $$",
+    " | $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$_____/  | $$ /$$",
+    " | $$$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/",
+    " |_______/  \______/  \_______/ \____  $$ \_______/   \___/",
+    "                                /$$  \ $$",
+    "                               |  $$$$$$/",
+    "                                \______/",
+    " /$$      /$$",
+    " | $$$    /$$$",
+    " | $$$$  /$$$$  /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$",
+    " | $$ $$/$$ $$ |____  $$| $$__  $$ |____  $$ /$$__  $$ /$$__  $$ /$$__  $$",
+    " | $$  $$$| $$  /$$$$$$$| $$  \ $$  /$$$$$$$| $$  \ $$| $$$$$$$$| $$  \__/",
+    " | $$\  $ | $$ /$$__  $$| $$  | $$ /$$__  $$| $$  | $$| $$_____/| $$",
+    " | $$ \/  | $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$",
+    " |__/     |__/ \_______/|__/  |__/ \_______/ \____  $$ \_______/|__/",
+    "                                             /$$  \ $$",
+    "                                            |  $$$$$$/",
+    "                                             \______/",
+    "           |---------------------------------|",
+    "           | VERSION 1.0 - Made by Kekosssss |",
+    "           |---------------------------------|"
+]
+TWO_LINE_TITLE_REQUIRED_WIDTH = max([len(TWO_LINE_TITLE[i]) for i in range(len(TWO_LINE_TITLE))])
+TWO_LINE_TITLE_REQUIRED_HEIGHT = len(TWO_LINE_TITLE)
+
+SHORT_TITLE = [
+    "    ___           _            _",
+    "   / __\_   _  __| | __ _  ___| |_",
+    "  /__\// | | |/ _` |/ _` |/ _ \ __|",
+    " / \/  \ |_| | (_| | (_| |  __/ |_",
+    " \_____/\__,_|\__,_|\__, |\___|\__|",
+    "                    |___/",
+    "   /\/\   __ _ _ __   __ _  __ _  ___ _ __",
+    "  /    \ / _` | '_ \ / _` |/ _` |/ _ \ '__|",
+    " / /\/\ \ (_| | | | | (_| | (_| |  __/ |",
+    " \/    \/\__,_|_| |_|\__,_|\__, |\___|_|",
+    "                           |___/",
+    "  |---------------------------------|",
+    "  | VERSION 1.0 - Made by Kekosssss |",
+    "  |---------------------------------|"
+]
+SHORT_TITLE_REQUIRED_WIDTH = max([len(SHORT_TITLE[i]) for i in range(len(SHORT_TITLE))])
+SHORT_TITLE_REQUIRED_HEIGHT = len(SHORT_TITLE)
+
+GOODBYE_TITLESCREEN = [
+    "   /$$$$$$                            /$$ /$$$$$$$",
+    "  /$$__  $$                          | $$| $$__  $$",
+    " | $$  \__/  /$$$$$$   /$$$$$$   /$$$$$$$| $$  \ $$ /$$   /$$  /$$$$$$",
+    " | $$ /$$$$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$  | $$ /$$__  $$",
+    " | $$|_  $$| $$  \ $$| $$  \ $$| $$  | $$| $$__  $$| $$  | $$| $$$$$$$$",
+    " | $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$  \ $$| $$  | $$| $$_____/",
+    " |  $$$$$$/|  $$$$$$/|  $$$$$$/|  $$$$$$$| $$$$$$$/|  $$$$$$$|  $$$$$$$",
+    " \______/  \______/  \______/  \_______/|_______/  \____  $$ \_______/",
+    "                                                   /$$  | $$",
+    "                                                  |  $$$$$$/",
+    "                                                   \______/"
+]
+GOODBYE_TITLESCREEN_REQUIRED_WIDTH = max([len(GOODBYE_TITLESCREEN[i]) for i in range(len(GOODBYE_TITLESCREEN))])
+GOODBYE_TITLESCREEN_REQUIRED_HEIGHT = len(GOODBYE_TITLESCREEN)
+
+########################################################################
+## MENU                                                               ##
+########################################################################
+MENU = [
+    "Available options :",
+    "     A - Generate and update all reports",
+    "     Z - Reports generation menu",
+    "     E - Display specific data",
+    "     R - Change Source Location",
+    "     T - Exit"
+]
+MENU_HEIGHT = len(MENU)
+
+########################################################################
+## PRINT FUNCTIONS                                                    ##
+########################################################################
+def print_line(line:str='', size=None):
+    if size == None:
+        size = os.get_terminal_size()
+    print(colored("# ", color='magenta', attrs=['bold']), end='')
+    print(colored(line, color='white', attrs=['bold']), end='')
+    for _ in range(size.columns-len(line)-3):
+        print(" ", end='')
+    print(colored("#", color='magenta', attrs=['bold']))
+
+def print_full_line(size=None):
+    if size == None:
+        size = os.get_terminal_size()
+    for _ in range(size.columns):
+        print(colored("#", color='magenta', attrs=['bold']), end='')
+    print('')
+
+def print_info_message(line):
+    print(colored(line, color='white', attrs=['bold']))
+
+def input_custom(line:str):
+    user_input = input(colored(line+"\n -> ", color='white', attrs=['bold']))
+    return user_input
+
+def print_title_line(i, size):
+    if ONE_LINE_TITLE_REQUIRED_WIDTH < size.columns-3 and ONE_LINE_TITLE_REQUIRED_HEIGHT < size.lines-3:
+        if i >= int(size.lines/2) - math.floor(ONE_LINE_TITLE_REQUIRED_HEIGHT/2) and i < int(size.lines/2) + math.ceil(ONE_LINE_TITLE_REQUIRED_HEIGHT/2):
+            print_line(line=ONE_LINE_TITLE[i-(int(size.lines/2) - math.floor(ONE_LINE_TITLE_REQUIRED_HEIGHT/2))], size=size)
+        else:
+            print_line(size=size)
+    elif TWO_LINE_TITLE_REQUIRED_WIDTH < size.columns-3 and TWO_LINE_TITLE_REQUIRED_HEIGHT < size.lines-3:
+        if i >= int(size.lines/2) - math.floor(TWO_LINE_TITLE_REQUIRED_HEIGHT/2) and i < int(size.lines/2) + math.ceil(TWO_LINE_TITLE_REQUIRED_HEIGHT/2):
+            print_line(line=TWO_LINE_TITLE[i-(int(size.lines/2) - math.floor(TWO_LINE_TITLE_REQUIRED_HEIGHT/2))], size=size)
+        else:
+            print_line(size=size)
+    else:
+        if i >= int(size.lines/2) - math.floor(SHORT_TITLE_REQUIRED_HEIGHT/2) and i < int(size.lines/2) + math.ceil(SHORT_TITLE_REQUIRED_HEIGHT/2):
+            print_line(line=SHORT_TITLE[i-(int(size.lines/2) - math.floor(SHORT_TITLE_REQUIRED_HEIGHT/2))], size=size)
+        else:
+            print_line(size=size)
+
+def print_title():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    size = os.get_terminal_size()
+    for i in range(size.lines-2):
+        if i == 0 or i == size.lines-3:
+            print_full_line(size=size)
+        else:
+            print_title_line(i, size)
+
+def print_menu_line(i, size):
+    if i >= 5 and i - 5 < MENU_HEIGHT:
+        print_line(line=MENU[i-5], size=size)
+    else:
+        print_line(size=size)
+
+def print_menu(source="Exemple/", name="Default"):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    size = os.get_terminal_size()
+    for i in range(size.lines-2):
+        if i == 0 or i == size.lines-3:
+            print_full_line(size=size)
+        elif i == 2:
+            print_line(line=f"Welcome {name} !", size=size)
+        elif i == 3:
+            print_line(line=f"Current source folder : {source}", size=size)
+        else:
+            print_menu_line(i, size)
+
+def print_goodbye_line(i, size):
+    if i >= int(size.lines/2) - math.floor(GOODBYE_TITLESCREEN_REQUIRED_HEIGHT/2) and i < int(size.lines/2) + math.ceil(GOODBYE_TITLESCREEN_REQUIRED_HEIGHT/2):
+        print_line(line=GOODBYE_TITLESCREEN[i-(int(size.lines/2) - math.floor(GOODBYE_TITLESCREEN_REQUIRED_HEIGHT/2))], size=size)
+    else:
+        print_line(size=size)
+
+def print_goodbye():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    size = os.get_terminal_size()
+    for i in range(size.lines-2):
+        if i == 0 or i == size.lines-3:
+            print_full_line(size=size)
+        else:
+            print_goodbye_line(i, size)
+
+########################################################################
+## DATA SORTING FUNCTIONS                                             ##
+########################################################################
+def assign_input(x:str):
+    x = x.capitalize()
+    match x:
+        case 'A' | '1':
+            return "ReportGeneration"
+        case 'T' | '5':
+            return "End"
+        case _:
+            return "Unknown"
+
+########################################################################
+## DATA MANAGEMENT FUNCTIONS                                          ##
+########################################################################
+def check_name_characters(name):
+    ##TODO: Verify name and return False as well as the incorrect characters if there is an issue
+    return True, ""
+
+def generate_all_reports(account:Account_manager, args):
+    print_menu(source=account.Folder_path, name=account.Name)
+    print_info_message("Reloading source data and updating subcategories data, please wait...")
+    account.update()
+    print_menu(source=account.Folder_path, name=account.Name)
+    print_info_message("Generating monthly reports, please wait...")
+    account.generate_monthly(summary_file_type=args.extension_format)
+    print_menu(source=account.Folder_path, name=account.Name)
+    print_info_message("Generating yearly reports, please wait...")
+    account.generate_yearly(summary_file_type=args.extension_format)
+    print_menu(source=account.Folder_path, name=account.Name)
+    print_info_message("Generating account summaries, please wait...")
+    account.generate_accounts_summary(summary_file_type=args.extension_format)
+
+########################################################################
+## TERMINAL PAGE FUNCTIONS                                            ##
+########################################################################
+def init_terminal():
+    print_title()
+    input_custom("Continue ?")
+
+def check_sources(args, previous_wrong=False):
+    if len(args.folders) == 0:
+        ## Add a source to use
+        print_title()
+        source = input_custom(f"Specify the path to your source folder{"" if previous_wrong==False else " (Previous result was not a valid path)"}")
+        if os.path.isdir(source) == False:
+            check_sources(args, previous_wrong=True)
+        else:
+            return source
+    elif len(args.folders) == 1:
+        return args.folders[0]
+    else:
+        ##TODO: Choose one of the sources (only one at a time is supported in interactive mode)
+        return args.folders[0]
+
+def check_name(args, previous_wrong=False, false_characters=""):
+    if len(args.name) == 0:
+        ## Add a name to use
+        print_title()
+        name = input_custom(f"Specify the name you want to use for this source{"" if previous_wrong==False else f" (Previous result contained forbiden characters : {false_characters})"}")
+        verdict, false_character = check_name_characters(name)
+        if verdict:
+            return name
+        else:
+            check_name(args, previous_wrong=True, false_characters=false_character)
+    elif len(args.name) == 1:
+        return args.name[0]
+    else:
+        ##TODO: Choose one of the names (only one at a time is supported in interactive mode)
+        return args.name[0]
+
+def use_option(option:str, account:Account_manager, args):
+    match option:
+        case "ReportGeneration":
+            generate_all_reports(account=account, args=args)
+        case "End":
+            return False
+        case "Unknown":
+            return True
+        case _:
+            assert False, f"UNKNOWN INPUT {option}"
+
+########################################################################
+## EXTERNAL USE FUNCTIONS                                             ##
+########################################################################
+def interactive_mode(args):
+    ## Initialisation of the program necessary values
+    init_terminal()
+    source = check_sources(args)
+    name = check_name(args)
+    account = Account_manager(source, name=name)
+    print_title()
+    print_info_message("Loading source folder and building up data, please wait...")
+    account.build()
+    ## Menu terminal
+    user_input = ""
+    option = "Start"
+    wrong_input = False
+    while option != "End":
+        print_menu(source=source, name=name)
+        user_input = input_custom(f"What do you want to do ? (A, Z, E, ...){"" if not wrong_input else f" (Previous input unrecognized : {user_input})"}")
+        option = assign_input(user_input)
+        wrong_input = use_option(option=option, account=account, args=args)
+    print_goodbye()
--- main.py
+++ main.py
@@ -2,33 +2,39 @@ import argparse
 from Classes.account_manager_class import Account_manager
 from Utils.global_var import *
 from Utils.logger import *
+from Utils.interactive_mode import interactive_mode
 
 def main(args):
     base_folder = os.getcwd()
     set_language_pack(args.language)
-    set_verbose_level(args.verbose)
-    ## Create Account managers
-    for i in range(len(args.folders)):
-        folder = args.folders[i]
-        assert os.path.isdir(folder), f"{folder} is not a valid directory path"
-        os.chdir(base_folder)
-        if (len(args.name) > i):
-            my_accounts = Account_manager(folder, name=args.name[i])
-        else:
-            if folder[-1] == '/':
-                name = folder.split('/')[-2]
+    if (args.interactive_mode):
+        interactive_mode(args)
+    else:
+        set_verbose_level(args.verbose)
+        ## Create Account managers
+        for i in range(len(args.folders)):
+            folder = args.folders[i]
+            assert os.path.isdir(folder), f"{folder} is not a valid directory path"
+            os.chdir(base_folder)
+            if (len(args.name) > i):
+                my_accounts = Account_manager(folder, name=args.name[i])
             else:
-                name = folder.split('/')[-1]
-            my_accounts = Account_manager(folder, name=name)
-        print_info(f"Account initialisation finished")
-        my_accounts.update(args.extension_format)
-        print_info(f"Account reports update finished")
-        my_accounts.generate_yearly(args.extension_format)
-        print_info(f"Account yearly reports finished")
-        my_accounts.generate_accounts_summary(args.extension_format)
-        print_info(f"Account general report finished")
-        my_accounts.display(depth=args.depth, show_empty_months_message=0)
-        print_info(f"Account display finished")
+                if folder[-1] == '/':
+                    name = folder.split('/')[-2]
+                else:
+                    name = folder.split('/')[-1]
+                my_accounts = Account_manager(folder, name=name)
+            print_info(f"Account initialisation finished")
+            my_accounts.update()
+            print_info(f"Account reports update finished")
+            my_accounts.generate_monthly(summary_file_type=args.extension_format)
+            print_info(f"Account monthly reports finished")
+            my_accounts.generate_yearly(args.extension_format)
+            print_info(f"Account yearly reports finished")
+            my_accounts.generate_accounts_summary(args.extension_format)
+            print_info(f"Account general report finished")
+            my_accounts.display(depth=args.depth, show_empty_months_message=0)
+            print_info(f"Account display finished")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(prog="AccountManager", description="Programm used to parse excel file containing transaction informations accross different accounts, years, months and categories. Also allows one to see summaries and generate them using a file format of their choice.")
@@ -38,5 +44,6 @@ if __name__=="__main__":
     parser.add_argument('-n', "--name", help="Overide for the account owner's names", action='extend', nargs='+', type=str, default=[])
     parser.add_argument('-l', "--language", help="Choose the language to use for parsing and summaries", type=str, default='Français', choices=['Français', 'English', 'English (USA)'])
     parser.add_argument('-e', "--extension_format", help="Choose the extension to use for output summaries", type=str, default='xlsx', choices=['xlsx'])
+    parser.add_argument('-i', "--interactive_mode", help="Used to access data in interactive mode", action='store_true')
     args = parser.parse_args()
     main(args)
